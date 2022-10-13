7635df61ff9867258011f3c9f8daa18bcbf3201b
Lucas Pires <lucaspires2807@gmail.com>
2022-10-13 11:24:08 -0300

    ExRat.hs and ExBox.hs

---
+++ ex/ExRat.hs
@@ -0,0 +1,41 @@
+module ExRat
+    ( rat
+    , (//)
+    , denominator
+    , numerator
+    ) where
+
+-- define Rat:
+data Rat = Rat Integer Integer
+    deriving (Eq)
+
+instance Show Rat where
+    show (Rat a b) = "Rat " ++ show a ++ " " ++ show b
+
+instance Eq Rat where
+    (==)
+
+instance Num Rat where
+    (Rat a b) + (Rat m n) = Rat (a*n + m*b) (b * n)
+    (Rat a b) * (Rat m n) = Rat (a * m) (b * n)
+    negate (Rat a b)      = Rat (-a) (b)
+    abs (Rat a b)         = Rat (abs(a)) (abs(b))
+    signum (Rat a b)      = if (a >= 0 && b > 0) then 1 else -1
+    fromInteger x = Rat x 1
+
+instance Ord Rat where
+    compare = undefined
+
+rat :: Integer -> Integer -> Rat
+rat m n | n == 0 = error "Denominator can't be zero!"
+        | otherwise = Rat m n
+
+(//) :: Rat -> Rat -> Rat
+(Rat a b) // (Rat m n) = Rat (a * n) (b * m)
+
+denominator :: Rat -> Integer
+denominator (Rat m n) = n
+
+numerator :: Rat -> Integer
+numerator (Rat m n) = n
+
